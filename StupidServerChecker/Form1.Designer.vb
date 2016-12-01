31d711747c5b5592cf247e935be6c39f474eadad
Noire <Noire@noire.cc>
2016-11-30 22:40:30 -0700

    added to git, SSCv3

---
+++ StupidServerChecker/Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Maplestory is:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Label2.Location = New System.Drawing.Point(52, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(209, 46)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Checking.."
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(45, 161)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(117, 24)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Start maple"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 15000
+        '
+        'BackgroundWorker1
+        '
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(220, 161)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(76, 24)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Alarm"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(419, 220)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.Red
+        Me.Name = "Form1"
+        Me.Text = "stupid server checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ StupidServerChecker/Form1.vb
@@ -0,0 +1,167 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Threading
+Imports System.Windows.Forms
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Runtime.InteropServices
+Public Class Form1
+    Dim MajorV As Integer
+    Dim MinorV As Integer
+    Dim mapleInst As New Process
+    Dim msdir As String
+    Dim last As Boolean = True
+    Dim current As Boolean
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Private Shared Function FindWindow(
+     ByVal lpClassName As String,
+     ByVal lpWindowName As String) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Private Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+
+    End Function
+    Private Const WM_SYSCOMMAND = &H112
+    Private Const SC_CLOSE = &HF060
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        msdir = findmsdir()
+        mapleInst.StartInfo.FileName = msdir & "\Maplestory.exe"
+        mapleInst.StartInfo.Arguments = "GameLaunching"
+        mapleInst.StartInfo.WorkingDirectory = msdir
+        CheckBox1.Checked = True
+        CheckBox2.Checked = True
+        Timer1.Start()
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Not BackgroundWorker1.IsBusy Then
+            BackgroundWorker1.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub startmaple()
+        mapleInst.Start()
+        Threading.Thread.Sleep(4000)
+        clickplay()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        current = isMapleUp()
+    End Sub
+
+    Private Sub bgw_done(ByVal sender As Object, ByVal e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        If e.Error IsNot Nothing Then
+            Label2.Text = "Error: " & e.Error.Message
+            GoTo endl
+
+        End If
+        If current = True Then
+            Label2.Text = "Online v" + MajorV.ToString + "." + MinorV
+        Else
+            Label2.Text = "Offline"
+        End If
+        If (current = Not last) And (current = True) Then
+            If CheckBox2.Checked = True Then
+                My.Computer.Audio.Play(My.Resources.alarm, AudioPlayMode.Background)
+            End If
+            If CheckBox1.Checked = True Then
+                startmaple()
+            End If
+        End If
+        last = current
+endl:
+
+    End Sub
+
+    Private Function isMapleUp()
+        'Try
+        '    Dim loginserv As TcpClient = New TcpClient("8.31.99.141", 8484)
+        '    current = loginserv.Connected
+        '    loginserv.Close()
+        '    Return current
+        'Catch
+        '    Return False
+        'End Try
+        Try
+            Dim serverStream As NetworkStream
+            Dim serverIP As IPAddress = IPAddress.Parse("8.31.99.141")
+            Dim port As Integer
+            Dim clientSocket As TcpClient = New TcpClient()
+            clientSocket.Connect(serverIP, port)
+            serverStream = clientSocket.GetStream()
+            Dim inStream(100000) As Byte
+            serverStream.Read(inStream, 0, clientSocket.ReceiveBufferSize)
+            MajorV = BitConverter.ToInt16(inStream, 2)
+            MinorV = Convert.ToInt16(getStingFromByteA(inStream, 4))
+            current = clientSocket.Connected
+            clientSocket.Close()
+            Return current
+        Catch
+            Return False
+        End Try
+
+
+
+    End Function
+    Private Function getStingFromByteA(byteArray As Byte(), Startbyte As Integer)
+        Dim characters As Integer = byteArray(Startbyte)
+        Dim output As String = String.Empty
+        For i As Integer = Startbyte + 2 To Startbyte + 1 + characters Step +1
+            output = output + Convert.ToChar(byteArray(i))
+        Next
+        Return output
+    End Function
+    Private Function findmsdir() As String
+        Dim dir As String = vbNull
+        Dim pass As Boolean = False
+
+        dir = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Wizet\MapleStory", "ExecPath", "C:\Nexon\Maplestory")
+        If My.Computer.FileSystem.FileExists(dir & "\maplestory.exe") Then
+            'MessageBox.Show("1 " & dir)
+            pass = True
+            GoTo found
+        End If
+        dir = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\MicroSoft\Windows\CurrentVersion\Uninstall\MapleStory", "InstallLocation", "C:\Nexon\Maplestory")
+        If My.Computer.FileSystem.FileExists(dir & "\maplestory.exe") Then
+            'MessageBox.Show("2 " & dir)
+
+            pass = True
+            GoTo found
+        End If
+        dir = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\NoireLoader", "maplestoryfolder", vbNull)
+        If My.Computer.FileSystem.FileExists(dir & "\maplestory.exe") Then
+            'MessageBox.Show("2 " & dir)
+
+            pass = True
+            GoTo found
+        End If
+        dir = "C:\Nexon\Maplestory"
+        If My.Computer.FileSystem.FileExists(dir & "\maplestory.exe") Then
+            'MessageBox.Show("3 " & dir)
+            pass = True
+            GoTo found
+        End If
+found:
+        If pass Then
+            'MessageBox.Show("returning: " & dir)
+            Return dir
+        Else
+
+            MessageBox.Show("MapleStory.exe not found")
+            Return "null"
+        End If
+
+    End Function
+
+    Private Sub clickplay()
+        Threading.Thread.Sleep(200)
+        Dim i As Long = FindWindow("StartUpDlgClass", "MapleStory")
+        'MessageBox.Show(i.ToString("X"))
+        'PostMessage(i, WM_CLOSE, CLng(0), CLng(0))
+        SendMessage(i, WM_SYSCOMMAND, SC_CLOSE, 0)
+    End Sub
+End Class
